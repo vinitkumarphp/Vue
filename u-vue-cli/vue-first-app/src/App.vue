61abc3146a353ac4e601916fbbb5849fbadfaca7
vinitkumarphp <vinit1988php@gmail.com>
2021-09-02 13:28:17 +0530

    props & methods send to parent chield component

--- u-vue-cli/vue-first-app/src/App.vue
+++ u-vue-cli/vue-first-app/src/App.vue
@@ -3,14 +3,29 @@
         <div class="card">
             <h1>{{name}}</h1>
             <div class="card-content">
-                <contact-list></contact-list>
-                <contact-list></contact-list>
+<!--                <contact-list></contact-list>-->
+<!--                <contact-list></contact-list>-->
+
+                <contact-form @add-contact="addContact"></contact-form>
             </div>
         </div>
 
         <div class="card">
-            <friends-list v-for="contact in  contacts" :key="contact.id" :name="contact.name" :email="contact.email"
-                          :phone="contact.phone"></friends-list>
+<!--            <friends-list v-for="contact in  contacts" :key="contact.id" :name="contact.name" :email="contact.email"-->
+<!--                          :phone="contact.phone" :isFab="contact.isFab"></friends-list>-->
+            <friends-list-props-validate v-for="contact in  contacts"
+                                         :id="contact.id"
+                                         :key="contact.id"
+                                         :name="contact.name"
+                                         :email="contact.email"
+                                         :phone="contact.phone"
+                                         :isFab="contact.isFab"
+                                         @toggle-favorite="setIsfavorite"
+                                        @delete="deleteContact">
+
+
+            </friends-list-props-validate>
+
         </div>
 
     </div>
@@ -27,23 +42,54 @@
                         id: 1,
                         name: "Raunak Kumar",
                         email: "Raunak@gmail.com",
-                        phone: "12313213"
+                        phone: "12313213",
+                        isFab:false
                     },
                     {
                         id: 2,
                         name: "Bhanu Kumar",
                         email: "Bhanu@gmail.com",
-                        phone: "12313213"
+                        phone: "12313213",
+                        isFab:true
                     },
                     {
                         id: 3,
                         name: "Rupesh Kumar",
                         email: "Rupesh@gmail.com",
-                        phone: "12313213"
+                        phone: "12313213",
+                        isFab:false
                     }
                 ]
 
             }
+        },
+        methods:{
+            setIsfavorite(favId){
+               // alert("HI"+favId);
+                const  identifyFriend= this.contacts.find(
+                    (concat)=>concat.id===favId
+                );
+                identifyFriend.isFab=! identifyFriend.isFab;
+
+            },
+            addContact(enteredName,enteredEmail,enteredPhone){
+
+                const newContact={
+                    id:this.contacts.length+1,//new Date().toISOString(),
+                    name:enteredName,
+                    email:enteredEmail,
+                    phone:enteredPhone,
+                    isFab:false
+                };
+                this.contacts.push(newContact);
+
+            },
+            deleteContact(contactId){
+
+                this.contacts=this.contacts.filter((contact) => contact.id != contactId);
+
+            }
         }
+
     }
 </script>
---
+++ u-vue-cli/vue-first-app/src/components/ContactForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <article class="message is-primary">
+        <div class="message-header">
+
+            Add Contact
+        </div>
+
+        <div class="message-body">
+       <form @submit.prevent="addContact">
+
+           <input v-model="enteredName" class="input is-primary mt-5" type="text" placeholder="Name">
+           <input v-model="enteredEmail" class="input is-primary mt-5" type="text" placeholder="Email">
+           <input v-model="enteredPhone" class="input is-primary mt-5" type="text" placeholder="Phone">
+
+           <input class="button  mt-5 is-success" type="submit" value="Submit input">
+       </form>
+   </div>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: "Contact Form",
+
+        emits:['add-contact'],
+        data(){
+                return{
+                    enteredName:"",
+                    enteredEmail:"",
+                    enteredPhone:"",
+                }
+        },
+        methods:{
+            addContact(){
+
+                this.$emit('add-contact',this.enteredName,this.enteredEmail,this.enteredPhone);
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
