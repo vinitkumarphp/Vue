5f033ebbe8a7df05ba4c4a7464e7e3014f7bad03
vinitkumarphp <vinit1988php@gmail.com>
2021-10-16 21:19:56 +0530

    new start with vue

---
+++ u-vue-cli/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ActiveUser"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ u-vue-cli/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "UserData"
+    }
+</script>
+
+<style scoped>
+
+</style>
